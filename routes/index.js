a2f8aa84c919ff6b840fc77fb13e99fc64725140
RaquelRB <raquelrobles95@gmail.com>
2020-12-09 12:03:41 +0100

    Added route allbooks

--- routes/index.js
+++ routes/index.js
@@ -1,70 +1,28 @@
 const express = require('express');
 const User = require('../models/User');
 const Book = require('../models/Book');
-const router  = express.Router();
+const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.send('Home')
 });
 
-// router.post('/leidos', (req, res)=>{
-//   User.findByIdAndUpdate(req.body.userID, {$push: {leidos: req.body.mangaID}})
-//   .then((result)=>{
-//     console.log(result)
-//   })
-//   .catch((err)=>{
-//     console.log(err)
-//   })
-// })
-
-// router.post('/leyendo', (req, res)=>{
-//   User.findByIdAndUpdate(req.body.userID, {$push: {leyendo: req.body.mangaID}})
-//   .then((result)=>{
-//     console.log(result)
-//   })
-//   .catch((err)=>{
-//     console.log(err)
-//   })
-// })
-
-// router.post('/porLeer', (req, res)=>{
-//   User.findByIdAndUpdate(req.body.userID, {$push: {porLeer: req.body.mangaID}})
-//   .then((result)=>{
-//     console.log(result)
-//   })
-//   .catch((err)=>{
-//     console.log(err)
-//   })
-// })
-
-router.get('/getUser/:id', (req, res)=>{
+router.get('/getUser/:id', (req, res) => {
 
   User.findById(req.params.id)
-  .then((result)=>{
-    res.send(result)
-  })
-  .catch((err)=>{
-    console.log(err)
-  })
+    .then((result) => {
+      res.send(result)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 })
 
-// router.get('/myBooks', (req, res) => {
-//   const userId = req.user._id
-//   const user = req.user
-
-//   User.findOne({_id: userId})
-//     .populate('myBooks')
-//     .then((result)=>{
-//       res.render('auth/myList', {boardgame_id: result.boardgame_id, user})
-//     })
-//     .catch((err)=>console.log(err))
-// })
-
 router.post('/myBooks', (req, res) => {
-  const {title, image, owner} = req.body
+  const { title, image, owner } = req.body
 
-  Book.create({title, image, owner})
+  Book.create({ title, image, owner })
     .then((createdBook) => {
       User.findByIdAndUpdate(owner, { $push: { myBooks: createdBook._id } })
         .then((result) => result)
@@ -75,13 +33,23 @@ router.post('/myBooks', (req, res) => {
 router.post('/showMyBooks', (req, res) => {
 
   const owner = req.body.owner
-  
+
   User.findById(owner)
-  .populate('myBooks')
-  .then((result)=>{
-    res.send(result)
-  })
-  .catch((err)=>console.log(err))
+    .populate('myBooks')
+    .then((result) => {
+      res.send(result)
+    })
+    .catch((err) => console.log(err))
+})
+
+router.post('/allBooks', (req, res) => {
+  Book.find({})
+  .populate('owner')
+    .then((result) => {
+      res.send(result)
+
+    })
+    .catch((err) => console.log(err))
 })
 
 module.exports = router;
